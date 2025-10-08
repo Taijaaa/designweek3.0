ac47b80fc656e73d5156cc3918237405622b4426
Brayden2025 <000969117@mohawkcollege.ca>
2025-10-08 12:27:11 -0400

    started implementing code and narrative

--- TextAdventureSample/TerminalGame.cs
+++ TextAdventureSample/TerminalGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace MohawkTerminalGame
 {
@@ -6,15 +7,15 @@ namespace MohawkTerminalGame
     {
         // Place your variables here
 
-
+        
         /// Run once before Execute begins
         public void Setup()
         {
             Program.TerminalExecuteMode = TerminalExecuteMode.ExecuteLoop;
             Program.TerminalInputMode = TerminalInputMode.KeyboardReadAndReadLine;
 
-            Terminal.SetTitle("Text Adventure Sample");
-            Terminal.RoboTypeIntervalMilliseconds = 50; // 50 milliseconds
+            Terminal.SetTitle("the heart of deception ");
+            Terminal.RoboTypeIntervalMilliseconds = 70; // 70 milliseconds
             Terminal.UseRoboType = true; // use slow character typing
             Terminal.WriteWithWordBreaks = true; // donbreak around wors, don't cut them off
             Terminal.WordBreakCharacter = ' '; // break on spaces
@@ -29,11 +30,22 @@ namespace MohawkTerminalGame
         {
             Terminal.RoboTypeIntervalMilliseconds = 50;
             Terminal.Beep();
-            Terminal.WriteLine("DANGER. AREA INFECTED WITH VIRUS.", ConsoleColor.Black, ConsoleColor.DarkYellow);
+            Terminal.WriteLine("This is your final test to prove yourself worthy to Omarious, the lead councilman of Angrulia. Many have tried before, but those who ventured out never returned." +
+                " Your task is to track down Helsadona" +
+                "the banished witch who dwells deep within the cave at the edge of the local forest and bring back her heart." +
+                "You walk alongside a forest elf guiding you toward your destination. He speaks of the town’s resentment toward Helsadona," +
+                "how many believe she is the cause of the havoc and chaos that plagues Angrulia. She was once a councilwoman of Angrulia, " +
+                "until a falling out with Omarious led to her banishment." +
+                "To this day no one knows what truly happened between them.As you reach the crumbling cave entrance, the elf turns to you." +
+                " “She is the most powerful being this town has ever seen. You will need something to defend yourself.” With a flick of his wrist, " +
+                "a golden glow appears before you. Within it floats three weapons: a scepter, a sword, and a spear. “Which one calls to you?”");
+            
+
             Terminal.Beep();
             Terminal.RoboTypeIntervalMilliseconds = 10;
-            Terminal.WriteLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-            Terminal.WriteLine("Do you wish to proceed? (y/n)", ConsoleColor.Black, ConsoleColor.Red);
+            // player option to choose a weapon  
+            Terminal.WriteLine("which weapon do you choose [septor], [sword], or a [spear]");
+            
             string answer = Terminal.ReadAndClearLine();
             if (answer.ToLower().Equals("y"))
             {
